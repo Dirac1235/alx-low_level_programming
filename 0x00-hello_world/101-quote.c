6b5363d2a6ece43b04c2a6b3bb2abc3fe44598c6
webi Muleta <webimuleta01@gmail.com>
2023-06-15 12:32:39 -0700

    New file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include<unistd.h>
+#include<stdio.h>
 /**
  * main - error printer
  * Return: 1
  */
 int main(void)
 {
-char message[60];
-message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
