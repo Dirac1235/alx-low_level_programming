2cdde4b3aa058bfee37ed303e373aadf621a799e
webi Muleta <webimuleta01@gmail.com>
2023-06-16 12:19:55 -0700

    New file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - one upto ten
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
