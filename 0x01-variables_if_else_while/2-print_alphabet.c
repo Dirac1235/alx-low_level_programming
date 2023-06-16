fea253f7b6d6cdcf516a7218a9fe98c0b0414ab0
webi Muleta <webimuleta01@gmail.com>
2023-06-16 11:17:25 -0700

    New file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - putchar
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int i = 97;
+
+	for (; i < 123 ; i++)
+	{
+		putchar (i);
+	}
+	putchar ('\n')
+	return (0);
+}
