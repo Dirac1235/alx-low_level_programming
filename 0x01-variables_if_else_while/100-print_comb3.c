b3f715bb8074be3577111ea7767e9dd7d6ddccff
webi Muleta <webimuleta01@gmail.com>
2023-06-16 12:38:36 -0700

    New file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,13 +16,13 @@ int main(void)
 		{
 			putchar((num % 10) + '0');
 			putchar((num1 % 10) + '0');
+			if (num1 == 9 && num == 9)
+			{
+				continue;
+			}
+			putchar (',');
+			putchar (' ');
 		}
-		if (num == 9)
-		{
-			continue;
-		}
-		putchar (',');
-		putchar (' ');
 	}
 	return (0);
 }
