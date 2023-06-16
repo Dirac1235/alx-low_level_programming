ccf02aea7e21450ee23c5f8b4a3d56cd2f6a833d
webi Muleta <webimuleta01@gmail.com>
2023-06-16 13:58:38 -0700

    New file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,24 +15,24 @@ int main(void)
 	{
 		for (num1 = num + 1; num1 < 10; num1++)
 		{
-			for(num2 = num1 + 1; num2 < 10; num2++)
+			for (num2 = num1 + 1; num2 < 10; num2++)
 			{
 				putchar ((num % 10) + '0');
 				putchar ((num1 % 10) + '0');
 				putchar ((num2 % 10) + '0');
-				if (num1 == 9 && num == 8 && num2 == 7)
+				if (num1 == 8 && num == 7 && num2 == 9)
 				{
 					break;
 				}
 				putchar (',');
 				putchar (' ');
 			}
-			if (num == 9 && num1 == 8 && num2 == 7)
+			if (num == 7 && num1 == 8 && num2 == 9)
 			{
 				break;
 			}
 		}
-		if (num == 8 && num1 == 9 && num2 == 7)
+		if (num == 7 && num1 == 8 && num2 == 9)
 		{
 			break;
 		}
