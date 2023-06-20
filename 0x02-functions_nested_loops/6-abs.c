f5f6663204c56df29385537111cb4988a93787a0
webi Muleta <webimuleta01@gmail.com>
2023-06-20 10:05:25 -0700

    New file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - prints small letter
+ * @c: The character to be checked.
+ * Return: postive or negative
+ *
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (-c);
+	}
+}
