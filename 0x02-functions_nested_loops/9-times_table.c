2baee90110aa02fb8dfdcc2c88c8bf6a488176cf
webi Muleta <webimuleta01@gmail.com>
2023-06-20 13:14:18 -0700

    New file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * times_table - prints small letter
+ *
+ * Return: void
+ *
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if ((i * j) > 9)
+			{
+				int b = (int) ((i * j) % 100) / 10;
+				_putchar (b + '0');
+				_putchar ((i * j) % 10 + '0');
+				if ((j + 1) != 10)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
+			}
+			else
+			{
+				_putchar ((i * j) + '0');
+				if ((i * (j + 1)) >= 10 && (j + 1) < 10)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+				else if ((j + 1) != 10)
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+				}
+			}
+		}
+		_putchar ('\n');
+	}
+}
