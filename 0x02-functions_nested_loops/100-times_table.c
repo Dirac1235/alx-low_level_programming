956d1f49e2533ee1180df4c30590ab369b9c77c0
webi Muleta <webimuleta01@gmail.com>
2023-06-20 14:49:14 -0700

    New file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar((m / 10) + '0');
 					_putchar((m % 10) + '0');
-				} else if (k >= 100)
+				} else if (m >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
