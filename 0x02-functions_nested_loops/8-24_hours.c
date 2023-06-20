d251d05d41d49eac7cec0d3da7db0a3711e0bf4e
webi Muleta <webimuleta01@gmail.com>
2023-06-20 11:56:42 -0700

    New file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * jack_bauer - prints small letter
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+
+	while (a != 2 || b != 4)
+	{
+		_putchar (a + '0');
+		_putchar (b + '0');
+		_putchar (':');
+		_putchar (c + '0');
+		_putchar (d + '0');
+		_putchar ('\n');
+		if (b == 9)
+		{
+			a += 1;
+			b = 0;
+		}
+		if (c == 5 && d == 9)
+		{
+			b += 1;
+		}
+		if (d == 9)
+		{
+			c += 1;
+		}
+		d += 1;
+		c = c % 6;
+		d = d % 10;
+	}
+}
