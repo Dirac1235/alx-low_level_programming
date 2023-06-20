16ef3772298053575c0ae594a67f1bf09d13e3ee
webi Muleta <webimuleta01@gmail.com>
2023-06-20 13:29:08 -0700

    New file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - prints small letter
+ * @n: The character to be checked.
+ * @b: The integer to be added
+ * Return: sum
+ *
+ */
+int add(int n, int b)
+{
+	return (n + b);
+}
