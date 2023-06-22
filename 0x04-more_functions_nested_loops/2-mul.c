9166edb44de4bb8beb005a5ceffb2888a31b198a
webi Muleta <webimuleta01@gmail.com>
2023-06-22 09:10:04 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - checks whether it is number,
+ * or not.
+ *
+ * @a: the first number.
+ * @b: the second number.
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
