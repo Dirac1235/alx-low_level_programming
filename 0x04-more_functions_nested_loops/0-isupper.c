cfc03e4b44204b606fd3fc18a5c26749d3afaff6
webi Muleta <webimuleta01@gmail.com>
2023-06-22 08:42:08 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+/**
+ * _isupper - checks whether it is uppercase,
+ * or lower case.
+ *
+ * @c: the test letter.
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	int a;
+
+	a = isupper(c);
+	return (a);
+}
