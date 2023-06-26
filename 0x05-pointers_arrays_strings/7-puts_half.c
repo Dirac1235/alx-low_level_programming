62d0c7ec5f2ccb3602c10f8430c182ea55595ea0
webi Muleta <webimuleta01@gmail.com>
2023-06-26 12:22:26 -0700

    New file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,10 +17,18 @@ void puts_half(char *str)
 	{
 		count++;
 	}
-	j = count / 2;
-	for (i = j; i < count; i++)
+	if (count % 2 == 0)
 	{
-		_putchar(str[i]);
+		for (i = count / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (count % 2)
+	{
+		for (j = (count - 1) / 2; j < count - 1; j++)
+		{
+			_putchar(str[j + 1]);
+		}
 	}
 	_putchar ('\n');
 }
