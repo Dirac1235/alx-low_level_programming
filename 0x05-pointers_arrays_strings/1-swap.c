4b25499c4436770e4077df96b26b211ab44d42f9
webi Muleta <webimuleta01@gmail.com>
2023-06-26 08:47:43 -0700

    New file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - updates the value of the variable
+ *
+ * @a: pointer to the first number
+ * @b: pointer to the second number
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	*a = temp;
+	*a = *b;
+	*b = temp;
+}
