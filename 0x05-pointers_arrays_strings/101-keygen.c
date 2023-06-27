4819dd14f2d03b76f256ed0ca2f49dabc9856adf
webi Muleta <webimuleta01@gmail.com>
2023-06-26 19:37:32 -0700

    New file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,15 +13,18 @@ int main(void)
 	int passw[100];
 	int i;
 	int max;
+	int sum;
 	int remaining;
 
 	srand(time(NULL));
 	max = 78;
+	sum = 0;
 
 	for (i = 0; i < 31; i++)
 	{
 		passw[i] = rand() % max;
 		putchar(passw[i] + '0');
+		sum += passw[i];
 	}
 	remaining = 2772 - sum;
 	remaining %= max;
