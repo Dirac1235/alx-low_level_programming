fab32ff3177cbc4ef70658b6a3a6d5a2e686038d
webi Muleta <webimuleta01@gmail.com>
2023-06-29 09:09:01 -0700

    New file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncat - copies the content of an array to another
+ *
+ * @dest: destination
+ * @src: source
+ * @n: how many will be copied
+ *
+ * Return: concatenated array
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0;
+	int count = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (count != n)
+	{
+		dest[i + count] = src[count];
+		count++;
+	}
+	dest[i + count] = '\0';
+	return (dest);
+}
