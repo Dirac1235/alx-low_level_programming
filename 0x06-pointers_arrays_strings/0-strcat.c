ccf69a7098668a23be553a8d6c842df5b57daf80
webi Muleta <webimuleta01@gmail.com>
2023-06-29 09:01:39 -0700

    New file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * _strcat - copies the content of an array to another
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return: concatenated array
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0;
+	int count = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[count] != '\0')
+	{
+		dest[i + count] = src[count];
+		count++;
+	}
+	dest[i + count] = '\0';
+	return (dest);
+}
