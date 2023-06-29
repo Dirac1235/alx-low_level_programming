a828c81c643fea0ab8d67ec958e0973c8f06ac36
webi Muleta <webimuleta01@gmail.com>
2023-06-29 09:13:57 -0700

    New file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,14 @@
  *
  * Return: concatenated array
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
 	int count = 0;
 
 	while (dest[i] != '\0')
 		i++;
-	while (count != n)
+	while (count <= n && src[count] != '\0')
 	{
 		dest[i + count] = src[count];
 		count++;
