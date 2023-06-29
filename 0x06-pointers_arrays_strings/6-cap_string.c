0958597553312829309402430df792b345e8510d
webi Muleta <webimuleta01@gmail.com>
2023-06-29 11:29:18 -0700

    New file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,19 +13,36 @@ char *cap_string(char *c)
 
 	while (c[count] != '\0')
 	{
+		if (count == 0 && c[count] >= 'a' && s[count] <= 'z')
+			count[count] -= 32;
+
 		if (c[count] >= 'a' && c[count] <= 'z')
 		{
 			if (c[count - 1] == ',' || c[count - 1] == ';' || c[count - 1] == '.')
+			{
 				a = (char) (c[count] - 32);
+				c[count] = a;
+			}
 			else if (c[count - 1] == '!' || c[count - 1] == '?' || c[count - 1] == '\"')
+			{
 				a = (char) (c[count] - 32);
+				c[count] = a;
+			}
 			else if (c[count - 1] == '(' || c[count - 1] == ')' || c[count - 1] == '{')
+			{
 				a = (char) (c[count] - 32);
+				c[count] = a;
+			}
 			else if (c[count - 1] == '}' || c[count - 1] == '\t')
+			{
 				a = (char) (c[count] - 32);
+				c[count] = a;
+			}
 			else if (c[count - 1] == ' ' || c[count - 1] == '\n')
+			{
 				a = (char) (c[count] - 32);
-			c[count] = a;
+				c[count] = a;
+			}
 		}
 		count++;
 	}
