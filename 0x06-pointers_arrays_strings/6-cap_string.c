1baa8380f1c47add62a4e20fb1416bc7b8482da8
webi Muleta <webimuleta01@gmail.com>
2023-06-29 11:12:12 -0700

    New file

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,30 +3,29 @@
  * cap_string - capitalizes words that come,
  * after special character
  *
- * @a: the string to be capitalized
+ * @c: the string to be capitalized
  * Return: the capitalized string
  */
-char *cap_string(char *a)
+char *cap_string(char *c)
 {
+	int count = 0;
+	char a;
+
 	while (c[count] != '\0')
 	{
 		if (c[count] >= 'a' && c[count] <= 'z')
 		{
 			if (c[count - 1] == ',' || c[count - 1] == ';' || c[count - 1] == '.')
 				a = (char) (c[count] - 32);
-				c[count] = a;
 			else if (c[count - 1] == '!' || c[count - 1] == '?' || c[count - 1] == '\"')
 				a = (char) (c[count] - 32);
-				c[count] = a;
 			else if (c[count - 1] == '(' || c[count - 1] == ')' || c[count - 1] == '{')
 				a = (char) (c[count] - 32);
-				c[count] = a;
 			else if (c[count - 1] == '}' || c[count - 1] == '\t')
 				a = (char) (c[count] - 32);
-				c[count] = a;
 			else if (c[count - 1] == ' ' || c[count - 1] == '\n')
 				a = (char) (c[count] - 32);
-				c[count] = a;
+			c[count] = a;
 		}
 		count++;
 	}
