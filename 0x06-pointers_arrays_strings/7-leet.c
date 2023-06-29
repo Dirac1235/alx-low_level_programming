ea1fc82d2a8bf45a052f1970be0bbbcf3eef9d21
webi Muleta <webimuleta01@gmail.com>
2023-06-29 11:35:29 -0700

    New file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+nclude "main.h"
+
+/**
+ * leet - encodes a string
+ * @c: string to be encoded
+ *
+ * Return: the string;
+ */
+char *leet(char *c)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = b[j];
+			}
+		}
+	}
+
+	return (c);
+}
