a2b9e80e1c4027f73a32a8f97f45e7ceb693f3f7
webi Muleta <webimuleta01@gmail.com>
2023-06-29 10:32:13 -0700

    New file

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,13 @@
 char *string_toupper(char *c)
 {
 	int count = 0;
-	int t;
 	char a;
 
 	while (c[count] != '\0')
 	{
 		if (c[count] >= 'a' && c[count] <= 'z')
 		{
-			a = c[count] - 32;
+			a = (char) (c[count] - 32);
 			c[count] = a;
 		}
 		count++;
