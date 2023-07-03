24052aab1a1ac6098c7f75e29a562c5a53711314
webi muleta <webimuleta01@gmail.com>
2023-07-03 17:39:40 +0300

    New file

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strspn - counts the  occurence in memory
+ *@s: the array
+ *@accept: charcter that is searched
+ *Return: the number of occurences
+ */
+ unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int count = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0';j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				count++;
+			}
+		}
+	}
+	return (count);
+}
