22f59f5fc2b0e97baac860da3db2b4f017709777
webi muleta <webimuleta01@gmail.com>
2023-07-05 17:51:45 +0300

    New file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
  * _puts_recursion - i writes with recursion
  * @s: the char to be written
  */
- void _puts_recursion(char *s)
- {
-        if (*s == '\0')
-        {
-            _putchar('\n');
-            return;
-        }
-        _putchar(*s);
-        _puts_recursion(s + 1);
- }
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
