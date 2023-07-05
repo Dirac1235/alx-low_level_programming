25fa941712ef9a69444d67c9a240a208b9f1f97b
webi muleta <webimuleta01@gmail.com>
2023-07-05 17:40:13 +0300

    New files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - i writes with recursion
+ * @s: the char to be written
+ */
+ void _puts_recursion(char *s)
+ {
+        if (*s == '\0')
+        {
+            _putchar('\n');
+            return;
+        }
+        _putchar(*s);
+        _puts_recursion(s + 1);
+ }
