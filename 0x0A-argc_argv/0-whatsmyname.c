14c428167eab06d888962dc2cc134d74a5a356f9
webi muleta <webimuleta01@gmail.com>
2023-07-07 22:09:34 +0300

    New File

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: zero
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n",*argv)
+	return(0)
+}
