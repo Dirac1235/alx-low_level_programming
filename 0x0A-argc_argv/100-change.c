c5ccb8ce75f14834cc349609b8a1342b5a93d7c5
webi muleta <webimuleta01@gmail.com>
2023-07-07 23:51:03 +0300

    New file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-/**
- * _atoi - prints the integer value of the first,
- * encounterd integer
- *
- * @str: pointer to the string
- *
- *Return: the integer
- */
-int _atoi(char *str)
-{
-	int index = 0;
-	int countNegatives = 0;
-	int result = 0;
-	int length = 0;
-	int foundDigit = 0;
-
-	while (*(str + length) != '\0')
-		length++;
-
-	for (index = 0; index < length && foundDigit == 0; index++)
-	{
-		if (*(str + index) == '-')
-			countNegatives++;
-
-		if (*(str + index) >= '0' && *(str + index) <= '9')
-		{
-			int digit = *(str + index) - '0';
-
-			if (countNegatives % 2)
-				digit = -digit;
-			result = result * 10 + digit;
-			foundDigit = 1;
-			if (*(str + index + 1) < '0' || *(str + index + 1) > '9')
-				break;
-			foundDigit = 0;
-		}
-	}
-
-	if (foundDigit == 0)
-		return (0);
-
-	return (result);
-}
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -63,12 +21,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (_atoi(argv[1]) < 0)
+	if (atoi(argv[1]) < 0)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
-	num = _atoi(argv[1]);
+	num = atoi(argv[1]);
 	for (i = 0; i < 5; i++)
 	{
 		sum += num / arr[i];
