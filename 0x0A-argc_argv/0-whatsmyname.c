1af20084849f2bbc8cfe50a830aabf21036bbf53
webi muleta <webimuleta01@gmail.com>
2023-07-07 22:10:58 +0300

    New file

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
  * main - prints the name of the file
+ * @argc: number of arguments
+ * @argv: array fof arguments
  *
  * Return: zero
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",*argv)
+	printf("%s\n", *argv)
 	return(0)
 }
