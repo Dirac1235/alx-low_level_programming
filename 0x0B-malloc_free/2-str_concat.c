a7c340b0813b42b9a7e8215545cd3f561bc2acf6
webi muleta <webimuleta01@gmail.com>
2023-07-10 22:33:39 +0300

    New file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,12 +25,12 @@ char *str_concat(char *s1, char *s2)
 	p = (char *) malloc(sizeof(char) * (len + len2 + 1));
 	if (p == NULL)
 		return (0);
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s1 != NULL)
 	{
 		*(p + i) = s1[i];
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2[j] != '\0' && s2 != NULL)
 	{
 		*(p + i + j) = s2[j];
 		j++;
