637ca95a16cd4fca1958d9ac75a0456f69966dce
webi muleta <webimuleta01@gmail.com>
2023-07-13 22:17:37 +0300

    New file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,15 +14,15 @@ int *array_range(int min, int max)
 	int i, size;
 
 	size = max - min + 1;
-	
+
 	if (min > max)
 		return (NULL);
-	
+
 	arr = malloc(sizeof(int) * size);
 
 	if (arr == NULL)
 		return (NULL);
-	
+
 	for (i = 0; min <= max; i++)
 	{
 		arr[i] = min++;
