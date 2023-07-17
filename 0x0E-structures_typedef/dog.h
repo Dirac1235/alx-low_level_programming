f4a98d265c9e2d94dcb7c351bb1f69670ba954f2
webi muleta <webimuleta01@gmail.com>
2023-07-17 19:04:06 +0300

    New file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - contains the information of the dog
+ * @name: carries name of the dog
+ * @age: carries age
+ * @owner: carries owner name
+ *
+ * Description: this structure containg the information about,
+ * the dog name the owner name and the age of the dog
+ */
+
 struct dog
 {
 	char *name;
