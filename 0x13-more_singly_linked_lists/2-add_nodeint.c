9039dfc871cf08175953da6117665922a03f9191
webi muleta <webimuleta01@gmail.com>
2023-08-01 17:30:59 +0300

    New file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,10 +13,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *temp;
 
+	temp = malloc(sizeof(listint_t));
 	if (!temp)
 		return (NULL);
-
-	temp = malloc(sizeof(listint_t));
 
 	temp->n = n;
 	temp->next = *head;
