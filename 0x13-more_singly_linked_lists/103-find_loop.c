36125ca798febb5119695f13341d8cb23012179e
webi muleta <webimuleta01@gmail.com>
2023-08-01 19:48:25 +0300

    New file

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop occurence
+ * @head: the list which may contain loop
+ * Return: the location if loop occurs
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *node = head;
+	listint_t *node_2 = head;
+
+	if (!head)
+		return (NULL);
+
+	while (node && node2 && node2->next)
+	{
+		node_2 = node_2->next->next;
+		node = node->next;
+		if (node_2 == node)
+		{
+			node = head;
+			while (node != node_2)
+			{
+				node = node->next;
+				node_2 = fast->next;
+			}
+			return (node_2);
+		}
+	}
+
+	return (NULL);
+}
