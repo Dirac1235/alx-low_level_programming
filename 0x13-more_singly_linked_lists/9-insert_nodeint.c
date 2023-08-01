0b6023bfe46af04b0f2992965ac4e47b8327b80b
webi muleta <webimuleta01@gmail.com>
2023-08-01 18:36:55 +0300

    New file

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,13 +22,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new || !head)
 		return (NULL);
 	new->n = n;
+	new->next = NULL;
 
-	for (i = 0; i < (idx - 1) && temp; i++)
+	if (idx == 0)
 	{
-		temp = temp->next;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	new->next = temp->next;
-	temp->next = new;
 
-	return (new);
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
 }
