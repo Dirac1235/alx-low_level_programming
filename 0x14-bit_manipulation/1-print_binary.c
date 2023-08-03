6640048c4386fd3c5ecdd3851d5a6221e258b767
webi muleta <webimuleta01@gmail.com>
2023-08-04 00:53:55 +0300

    New file

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+include "main.h"
+
+/**
+ * print_binary - it prints the binary representation of decimal
+ * @n: The number to be changed to  binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
