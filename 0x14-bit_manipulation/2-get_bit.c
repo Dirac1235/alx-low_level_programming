9a5944b0964e90fca339a46f753b806513939102
webi muleta <webimuleta01@gmail.com>
2023-08-04 17:46:44 +0300

    New file

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - it finds the bit of decimal
+ * at given index
+ * @n: The decimal number
+ * @index: the index to bit
+ *
+ * Return: the bit at that index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	if (index > 64)
+		return (-1);
+
+	bit = (n >> index) && 1;
+	return (bit);
+}
