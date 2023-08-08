4fc84978225f7c086da029eb8e95a3d028ea5dbd
webi muleta <webimuleta01@gmail.com>
2023-08-08 03:49:23 +0300

    New file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,15 +15,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-	for (; *(text_content + length) != '\0'; length++)
-		;
-
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (; *(text_content + length) != '\0'; length++)
+		;
 
 	write(fd, text_content, length);
 
