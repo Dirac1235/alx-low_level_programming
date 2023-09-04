113c41cfa90534ec32fa41b2c33384fc585403c6
Webi Muleta <webimuleta01@gmail.com>
2023-09-04 21:10:19 +0300

    First Commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - It reads from file given by filename
+ * @filename: The file we read
+ * @letters: The number of charachters it should read
+ * Return: The actual numbers of chars it could read
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t some_num;
+	int fd;
+	int file_read;
+	char *buffer = NULL;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters * sizeof(char));
+	if (!buffer)
+		return (0);
+
+	file_read = read(fd, buffer, letters);
+	if (file_read == -1)
+		return (0);
+
+	if (write(STDOUT_FILENO, buffer, file_read) != file_read)
+		return (0);
+
+	free(buffer);
+	close(fd);
+	return (file_read);
+}
