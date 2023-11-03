30f8b157518fe23bf0d1b23d90c3f1039a62240a
Webi Muleta <webimuleta01@gmail.com>
2023-11-03 18:19:08 +0300

    Project files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,21 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new = NULL;
+	hash_table_t *table = NULL;
 
 	if (size == 0)
 		return (NULL);
-	new = malloc(sizeof(hash_table_t));
-	if (new == NULL)
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
 		return (NULL);
-	new->size = size;
-	new->array = malloc(sizeof(hash_node_t *) * size);
-	if (new->array == NULL)
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
 		return (NULL);
-	return (new);
+
+	return (table);
 }
