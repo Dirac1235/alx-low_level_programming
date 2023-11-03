55bc2818d7c25edf482303b07729b23a1755ea17
Webi Muleta <webimuleta01@gmail.com>
2023-11-03 19:22:53 +0300

    Project file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,82 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - inserts data into the hash table
+ * @ht: the hashtable which we insert data into
+ * @key: the data to be translated to index by the hash function
+ * @value: is the data that is ascociated with the key
+ *
+ * Return: 1 if success 0 if not
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index;
+	hash_node_t *new_node;
+	hash_node_t *current_node;
+
+	if (ht == NULL || key == NULL || value == NULL || strcmp(key, "") == 0)
+		return (0);
+
+	index = key_index((unsigned char *) key, ht->size);
+	current_node = ht->array[index];
+
+	for (; current_node != NULL; current_node = current_node->next)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			if (strcmp(current_node->value, value) != 0)
+			{
+				free(current_node->value);
+				current_node->value = strdup(value);
+			}
+		return (1);
+		}
+	}
+
+	new_node = c_node(key, value);
+	if (new_node == NULL)
+		return (0);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
+
+/**
+ * c_node - function that creates a new_node node.
+ * @key: pointer to key.
+ * @value: pointer to value of the key.
+ *
+ * Return: pointer to new_node node else NULL.
+ */
+hash_node_t *c_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	if (key == NULL || strcmp(key, "") == 0 || value == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	return (new_node);
+}
