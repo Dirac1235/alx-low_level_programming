d78c6dc3c5f891d8b37171ff16a28f903fc81753
webi muleta <webimuleta01@gmail.com>
2024-02-06 10:40:52 +0300

    Project file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searchs for value using the linear
+ * algorithm
+ * @array: The list of numbers we perform linear search
+ * @size: The size of the array
+ * @value: The value we search for in the list
+ *
+ * Return: The index of the value where the number is found else -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array [%d] = [%d]", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#IFNDEF SEARCH_ALGOS_H
+#DEFINE SEARCH_ALGOS_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#ENDIF
